c563b644fc04f3c2387b9ea8b3e81b0235fed258
Revolution <169168790+JING00544@users.noreply.github.com>
2025-08-22 23:54:35 +1000

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,8 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import JSON from './components/JSON.vue'
 import './style.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
-const app = createApp(App)
-app.component('JSON', JSON)
-app.mount('#app')
+
+createApp(App).mount('#app')
