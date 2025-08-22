8222cd6b54007e201841ab311886823af347aeaf
Revolution <169168790+JING00544@users.noreply.github.com>
2025-08-22 22:48:13 +1000

    Activity 3.1 completed

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,9 @@
 <script setup>
-import JSON from './components/JSON.vue';
+// import JSON from './components/JSON.vue';
+import Form from './components/Form.vue';
 </script>
 
 <template>
-  <div id="app">
-    <JSON />
-  </div>
+  <Form />
 </template>
 
-<style scoped>
-#app {
-  margin: 0;
-  padding: 0;
-  min-height: 100vh;
-}
-</style>
