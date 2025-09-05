1f852b93f3958648cd0daf922f360126835b589c
jgao0062 <jgao0062@student.monash.edu>
2025-09-05 21:13:32 +1000

    Week 5 Lab setup

--- src/main.js
+++ src/main.js
@@ -1,14 +1,19 @@
+// import './assets/main.css'
+// import '@/assets/style.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
 import { createApp } from 'vue'
 import App from './App.vue'
-import './style.css'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import PrimeVue from 'primevue/config';
-import Aura from '@primeuix/themes/aura';
-import DataTable from 'primevue/datatable';
-import Column from 'primevue/column';
-import ColumnGroup from 'primevue/columngroup';   // optional
-import Row from 'primevue/row';                   // optional
 
-const app = createApp(App);
-app.use(PrimeVue, { ripple: true });
-app.mount('#app');
+import PrimeVue from 'primevue/config'
+import Aura from '@primevue/themes/aura'
+// import DataTable from 'primevue/datatable'
+// import Column from 'primevue/Column'
+
+const app = createApp(App)
+app.use(PrimeVue, { theme: { preset: Aura } })
+
+// app.component('DataTable', DataTable)
+// app.component('Column', Column)
+
+app.mount('#app')
